5c705929cf307841cbd4a501d869027c3213186e
Kevin Erkelenz <fanatixgames@gmail.com>
2022-01-29 08:38:36 -0800

    Added final page submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const pages = ["page1", "page2", "page3", "page4", "page5", "page6", "page7", "page8", "page9"];
+const pages = ["page1", "page2", "page3", "page4", "page5", "page6", "page7", "page8", "page9", "page10", "page11"];
 let currentPage = 0;
 
 function pageFlip() {
