a6ffe497ae8d728fb566fa05d55175876e615366
Kevin Erkelenz <fanatixgames@gmail.com>
2022-01-28 20:39:22 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const pages = ["page1", "page2", "page3", "page4", "page5", "page6"];
+let currentPage = 0;
+
+function pageFlip() {
+    var current = document.getElementById(pages[currentPage++]);
+    if (pages[currentPage]) {
+        var next = document.getElementById(pages[currentPage]);
+    } else {
+        currentPage = 0;
+        var next = document.getElementById(pages[currentPage]);
+}
+current.style.display = 'none';
+next.style.display = 'block';
+}
+
+function pageBack() {
+    var current = document.getElementById(pages[currentPage--]);
+    if (pages[currentPage]) {
+        var next = document.getElementById(pages[currentPage]);
+    } else {
+        currentPage = 0;
+        var next = document.getElementById(pages[currentPage]);
+}
+current.style.display = 'none';
+next.style.display = 'block';
+}
